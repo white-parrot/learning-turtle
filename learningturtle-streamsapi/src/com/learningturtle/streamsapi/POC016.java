73b878aca18d34e3809edddc90ed2f2bb696fe5e
white-parrot <everydaycodestory@gmail.com>
2023-10-12 03:08:01 +0530

    LT-1210230303 : This Commit is to Update remaining List of Programs

--- learningturtle-streamsapi/src/com/learningturtle/streamsapi/POC016.java
+++ learningturtle-streamsapi/src/com/learningturtle/streamsapi/POC016.java
@@ -8,12 +8,9 @@
 package com.learningturtle.streamsapi;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
